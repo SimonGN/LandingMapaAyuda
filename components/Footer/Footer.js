42d12ded4712cb2e2a4067c0c3e00d3f950f8c71
SimonGN <simongonzaleznegrete90@gmail.com>
2019-11-19 16:53:18 +0100

    init component footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,6 +7,7 @@ import ParagraphBody from "../../styles/fontsStyles/paragraphBody";
 
 
 import { i18n, withTranslation } from '../../i18n'
+import Button from "../Button/Button";
 
 const changeLanguage = (language, setLangauge) => {
     i18n.changeLanguage(language)
@@ -17,10 +18,25 @@ const changeLanguage = (language, setLangauge) => {
 const Footer = props => {
     const { t } = props;
     const [language, setLangauge] = useState(i18n.language)
-    const email = t("email");
-    const phone = t("phone");
     return (
         <FooterStyle >
+            <div className="green">
+                <ParagraphTitle align="center">{t("intro")}</ParagraphTitle>
+                <ParagraphTitle align="center">{t("intro")}</ParagraphTitle>
+                <Button/>
+            </div>
+            <div className="greenOpacity">
+                <div>
+                    <img className="iconWord" src="/static/svg/iconWord.svg" />
+                    <ParagraphTitle align="center">{t("intro")}</ParagraphTitle>
+                </div>
+                <div>
+                    <div>
+                        <Link href="/about"><ParagraphBody >{t("nav1")}</ParagraphBody></Link>
+                        <Link href="/about"><ParagraphBody >{t("nav2")}</ParagraphBody></Link>
+                    </div>
+                </div>
+            </div>
             
 
         </FooterStyle>
