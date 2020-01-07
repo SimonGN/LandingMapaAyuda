9e1c991acbeda9ab086fc4dd5a3af2247004bd15
SimonGN <simongonzaleznegrete90@gmail.com>
2020-01-07 18:26:43 +0100

    finish change home

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2d138916002b55a1f929";
+/******/ 	var hotCurrentHash = "98e89fb4b8c1ef953e1d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
