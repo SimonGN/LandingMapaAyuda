7455738cc04487e9176c2f018d4f51adb921eaf8
Rubén Villar Grela <ruben@rubenvillar.net>
2020-02-20 19:44:55 +0100

    created file for login styles

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Router from 'next/router'
 import Meta from "../components/Meta";
 import Nav from "../components/Nav/Nav";
-import styled from 'styled-components'
 import { FullScreenContainer, Card, Button, Input, Checkbox } from '../components'
 import googleLogo from '../static/svg/icon-google-login.svg'
 import { login, setFetching } from '../redux/actions/auth'
@@ -16,7 +15,7 @@ import ParagraphBodyRegular from '../styles/fontsStyles/ParagraphBodyRegular'
 import _ from 'lodash';
 import { withTranslation } from '../i18n'
 import { withTheme } from 'styled-components'
-
+import LoginStyle from '../styles/pageStyles/loginStyle'
 function Login({t, theme}) {
   
   const [forgottenPassword, setForgottenPassword] = useState(false)
@@ -195,88 +194,6 @@ function Login({t, theme}) {
     </React.Fragment>
   );
 }
-
-
-const LoginStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 384px;
-  width: 384px;
-  & .title{
-      text-align: center;
-      margin-bottom: 2rem;
-      font-size: 32px;
-      color: ${({theme}) => theme.colors.turquoise};
-      font-family: "PanaGT–Regular";
-    }
-  .social-buttons{
-    display: flex;
-    justify-content: space-between;
-    & button{
-      display: flex;
-      align-items: center;
-      width: calc(50% - 0.25rem);
-      & img {
-        margin-right: 0.5rem;
-      }
-      &:first-child{
-        margin-right: 0.5rem;
-      }
-    }
-  }
-  .separator {
-    position: relative;
-    margin-top: 2rem;
-    border-top: 1px solid ${({theme}) => theme.colors.dark05};
-    div{
-      font-size: 12px;
-      font-family: "PanaGT–Regular";
-      color: ${({theme}) => theme.colors.dark60};
-      background-color: #fff;
-      margin: auto;
-      width: 3rem;
-      text-align: center;
-      position: absolute;
-      left: calc(50% - 1.5rem);
-      top: -10px;
-    }
-  }
-
-  .login-form {
-    margin-top: 1rem;
-    & .bottom-section {
-      display: flex;
-      justify-content: space-between;
-      margin: 1rem 1rem 1.5rem 1rem;
-      & span {
-        color:  ${({theme}) => theme.colors.dark40};
-        font-size: 12px;
-        font-family: "PanaGT–Regular";
-        cursor: pointer;
-        &:hover{
-          color: ${({theme}) => theme.colors.green};
-          text-decoration: underline;
-        }
-      }
-    }
-  }
-
-  .form-bottom{
-    margin-top: 1rem;
-    text-align: center;
-    color: ${({theme}) => theme.colors.dark60};
-    font-family: "PanaGT–Regular";
-    font-size: 12px;
-    & .recover-password-message{
-      margin-bottom: 1rem;
-    }
-    & span {
-      color: ${({theme}) => theme.colors.green};
-      text-decoration: underline;
-      cursor: pointer;
-    }
-  }
-`
 
 Login.getInitialProps = async () => ({
   namespacesRequired: ["login"]
